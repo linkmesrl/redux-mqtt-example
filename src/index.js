f17d3a456339a15abf83d7a196bae92570cf52e6
Unknown <lorenzo.boasso@gmail.com>
2017-08-03 11:22:54 +0200

    implement initialization options clean code

--- src/index.js
+++ src/index.js
@@ -13,20 +13,6 @@ import { App } from 'containers';
 import theme from './config/theme';
 import registerServiceWorker from './config/registerServiceWorker';
 
-const client = MQTT.connect('ws://broker.mqttdashboard.com:8000/mqtt');
-
-const doStuff = () => {
-  console.log('ciao');
-  client.publish('asdzxcqwe/1', '!!!!It works!')
-  .then(() => console.log('Done'))
-  .catch(() => console.log('Err'));
-};
-
-// When passing async functions as event listeners, make sure to have a try catch block
-client.on('connect', doStuff);
-client.publish('asdzxcqwe/1', 'hello world!');
-
-
 const logger = store => next => (action) => {
   console.log('dispatching', action);
   const result = next(action);
@@ -34,19 +20,18 @@ const logger = store => next => (action) => {
   return result;
 };
 
-const vanillaPromise = store => next => (action) => {
-  if (typeof action.then !== 'function') {
-    console.log('Promise solved');
-    return next(action);
-  }
-  console.log('Dispatch promise');
-  return Promise.resolve(action).then(store.dispatch);
+const mqttPromise = (config) => {
+  // When passing async functions as event listeners, make sure to have a try catch block
+  // client.on('connect', doStuff);
+
+  const client = MQTT.connect(config);
+  return () => next => (action) => {
+    Promise.resolve(client.publish(action.topic, JSON.stringify(action))).then(next(action));
+  };
 };
 
-const mqttPromise = store => next => action => Promise.resolve(client.publish('asdzxcqwe/1', '!!!!It works!')).then(next(action));
-
 const store = createStore(reducer, composeWithDevTools(
-  applyMiddleware(logger, mqttPromise),
+  applyMiddleware(logger, mqttPromise('tcp://localhost:3000')),
   // other store enhancers if any
 ));
 
